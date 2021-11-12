d3895f1bb9c9fe9619284ab7e8febe870ccef2e9
anhth34 <tranhunganh197@gmail.com>
2021-11-12 21:56:52 +0700

    mock-back-end

---
+++ mock-back-end/models/Comment.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var CommentSchema = new mongoose.Schema({
+  body: String,
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  article: { type: mongoose.Schema.Types.ObjectId, ref: 'Article' }
+}, {timestamps: true});
+
+// Requires population of author
+CommentSchema.methods.toJSONFor = function(user){
+  return {
+    _id: this._id,
+    body: this.body,
+    createdAt: this.createdAt,
+    author: this.author,
+    article: this.article,
+  };
+};
+
+mongoose.model('Comment', CommentSchema);
